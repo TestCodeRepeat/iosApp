9ded637eabfbd39fd400bbd2e32f6db9d4aa7df0
flyingobjex <steve@flyingobjex.com>
2024-05-03 15:06:15 +0100

    Initial Commit

---
+++ iosApp/iosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by objex on 03/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
