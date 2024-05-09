25da038944d96c66bd608daa3a29967eabc573b6
flyingobjex <steve@flyingobjex.com>
2024-05-09 16:24:23 +0100

    build(*): wip sentry

--- iosApp/SentryImplIos.swift
+++ iosApp/SentryImplIos.swift
@@ -6,7 +6,38 @@
 //
 
 import Foundation
+import Sentry
+import shared
 
-class Sentry {
+class SentryImplIos:SentryShared {
     
+    static let instance = SentryImplIos()
+    
+    func captureException(e: KotlinException) {
+        SentrySDK.capture(exception: convertKotlinExceptionToNSException(e))
+    }
+    
+    func captureMessage(message:String){
+        SentrySDK.capture(message: message)
+    }
 }
+
+/// Converts a KotlinException to an NSException.
+/// - Parameter kotlinException: The KotlinException instance captured from Kotlin Multiplatform code.
+/// - Returns: An NSException that represents the same error.
+private func convertKotlinExceptionToNSException(_ kotlinException: KotlinException) -> NSException {
+    let message = kotlinException.message ?? "An unknown error occurred"
+
+    // Create userInfo dictionary to include any additional info you might need
+    let userInfo: [String: Any] = [
+        "KotlinStackTrace": kotlinException.cause as Any
+    ]
+
+    // Return an NSException with the extracted details
+    return NSException(
+        name: NSExceptionName(rawValue: "KotlinException"),
+        reason: message,
+        userInfo: userInfo
+    )
+}
+
